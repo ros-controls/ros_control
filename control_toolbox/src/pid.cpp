707b690b35b3205039d8af6f2db35d4ca0a2aef2
Jonathan Bohren <jbo@jhu.edu>
2013-02-05 12:14:34 -0500

    Adding Bob's fixes to the backwards-compatibility API

--- control_toolbox/src/pid.cpp
+++ control_toolbox/src/pid.cpp
@@ -159,7 +159,7 @@ double Pid::setError(double error, ros::Duration dt)
 
 double Pid::updatePid(double error, ros::Duration dt)
 {
-  double p_term, d_term, i_term;
+  double p_term, d_term;
   p_error_ = error; //this is pError = pState-pTarget
 
   if (dt == ros::Duration(0.0) || std::isnan(error) || std::isinf(error))
@@ -168,23 +168,11 @@ double Pid::updatePid(double error, ros::Duration dt)
   // Calculate proportional contribution to command
   p_term = p_gain_ * p_error_;
 
-  // Calculate the integral error
-  i_error_ = i_error_ + dt.toSec() * p_error_;
-
   //Calculate integral contribution to command
-  i_term = i_gain_ * i_error_;
+  i_term_ = i_term_ + i_gain_ * dt.toSec() * p_error_;
 
-  // Limit i_term so that the limit is meaningful in the output
-  if (i_term > i_max_)
-  {
-    i_term = i_max_;
-    i_error_=i_term/i_gain_;
-  }
-  else if (i_term < i_min_)
-  {
-    i_term = i_min_;
-    i_error_=i_term/i_gain_;
-  }
+  // Limit i_term_ so that the limit is meaningful in the output
+  i_term_ = std::max( i_min_, std::min( i_term_, i_max_) );
 
   // Calculate the derivative error
   if (dt.toSec() > 0.0)
@@ -227,33 +215,22 @@ double Pid::setError(double error, double error_dot, ros::Duration dt)
 
 double Pid::updatePid(double error, double error_dot, ros::Duration dt)
 {
-  double p_term, d_term, i_term;
+  double p_term, d_term;
   p_error_ = error; //this is pError = pState-pTarget
   d_error_ = error_dot;
 
   if (dt == ros::Duration(0.0) || std::isnan(error) || std::isinf(error) || std::isnan(error_dot) || std::isinf(error_dot))
     return 0.0;
 
+
   // Calculate proportional contribution to command
   p_term = p_gain_ * p_error_;
 
-  // Calculate the integral error
-  i_error_ = i_error_ + dt.toSec() * p_error_;
-
   //Calculate integral contribution to command
-  i_term = i_gain_ * i_error_;
+  i_term_ = i_term_ + i_gain_ * dt.toSec() * p_error_;
 
-  // Limit i_term so that the limit is meaningful in the output
-  if (i_term > i_max_)
-  {
-    i_term = i_max_;
-    i_error_=i_term/i_gain_;
-  }
-  else if (i_term < i_min_)
-  {
-    i_term = i_min_;
-    i_error_=i_term/i_gain_;
-  }
+  // Limit i_term_ so that the limit is meaningful in the output
+  i_term_ = std::max( i_min_, std::min( i_term_, i_max_) );
 
   // Calculate derivative contribution to command
   d_term = d_gain_ * d_error_;
