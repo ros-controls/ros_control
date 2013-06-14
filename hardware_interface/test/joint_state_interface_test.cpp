0afaba9fefb2536d0249eaaa458503c92259cbd9
Adolfo Rodriguez Tsouroukdissian <adolfo.rodriguez@pal-robotics.com>
2013-06-14 20:11:31 +0200

    Unit test hardware_interfaces.

---
+++ hardware_interface/test/joint_state_interface_test.cpp
@@ -0,0 +1,93 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2013, PAL Robotics S.L.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of hiDOF, Inc. nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//////////////////////////////////////////////////////////////////////////////
+
+/// \author Adolfo Rodriguez Tsouroukdissian
+
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+#include <hardware_interface/joint_state_interface.h>
+
+using std::string;
+using namespace hardware_interface;
+
+class JointStateInterfaceTest : public ::testing::Test
+{
+public:
+  JointStateInterfaceTest()
+    : pos1(1.0), vel1(2.0), eff1(3.0),
+      pos2(4.0), vel2(5.0), eff2(6.0),
+      name1("name_1"),
+      name2("name_2"),
+      h1(name1, &pos1, &vel1, &eff1),
+      h2(name2, &pos2, &vel2, &eff2)
+  {}
+
+protected:
+  double pos1, vel1, eff1;
+  double pos2, vel2, eff2;
+  string name1;
+  string name2;
+  JointStateHandle h1, h2;
+};
+
+TEST_F(JointStateInterfaceTest, ExcerciseApi)
+{
+  JointStateInterface iface;
+  iface.registerHandle(h1);
+  iface.registerHandle(h2);
+
+  // Get handles
+  EXPECT_NO_THROW(iface.getHandle(name1));
+  EXPECT_NO_THROW(iface.getHandle(name2));
+
+  JointStateHandle h1_tmp = iface.getHandle(name1);
+  EXPECT_EQ(name1, h1_tmp.getName());
+  EXPECT_DOUBLE_EQ(pos1, h1_tmp.getPosition());
+  EXPECT_DOUBLE_EQ(vel1, h1_tmp.getVelocity());
+  EXPECT_DOUBLE_EQ(eff1, h1_tmp.getEffort());
+
+  JointStateHandle h2_tmp = iface.getHandle(name2);
+  EXPECT_EQ(name2, h2_tmp.getName());
+  EXPECT_DOUBLE_EQ(pos2, h2_tmp.getPosition());
+  EXPECT_DOUBLE_EQ(vel2, h2_tmp.getVelocity());
+  EXPECT_DOUBLE_EQ(eff2, h2_tmp.getEffort());
+
+  // This interface does not claim resources
+  EXPECT_TRUE(iface.getClaims().empty());
+
+  // Print error message
+  // Requires manual output inspection, but exception message should contain the interface name (not its base clase)
+  try {iface.getHandle("unknown_name");}
+  catch(const HardwareInterfaceException& e) {std::cout << e.what() << std::endl;}
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ hardware_interface/test/resource_manager_test.cpp
@@ -0,0 +1,188 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2013, PAL Robotics S.L.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of hiDOF, Inc. nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//////////////////////////////////////////////////////////////////////////////
+
+/// \author Adolfo Rodriguez Tsouroukdissian
+
+#include <algorithm>
+#include <set>
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include <hardware_interface/resource_manager.h>
+
+using std::find;
+using std::string;
+using std::vector;
+using std::set;
+
+using namespace hardware_interface;
+
+class HandleType
+{
+public:
+  HandleType(const string& name, int val) : name_(name), val_(val) {}
+  string getName() const {return name_;}
+  int getValue() {return val_;}
+private:
+  int val_;
+  string name_;
+};
+
+class ResourceManagerTest : public ::testing::Test
+{
+public:
+
+
+  ResourceManagerTest()
+    : h1("resource1", 1),
+      h2("resource2", 2) {}
+
+protected:
+  HandleType h1, h2;
+};
+
+TEST_F(ResourceManagerTest, ExcerciseApi)
+{
+  // Populate resource manager
+  ResourceManager<HandleType> mgr;
+  ASSERT_TRUE(mgr.getNames().empty());
+
+  mgr.registerHandle(h1);
+  mgr.registerHandle(h2);
+
+  // Joint names
+  vector<string> names = mgr.getNames();
+  EXPECT_EQ(2, names.size());
+  EXPECT_TRUE(find(names.begin(), names.end(), h1.getName()) != names.end());
+  EXPECT_TRUE(find(names.begin(), names.end(), h2.getName()) != names.end());
+
+  // Get handles
+  EXPECT_NO_THROW(mgr.getHandle(h1.getName()));
+  EXPECT_NO_THROW(mgr.getHandle(h2.getName()));
+
+  HandleType h1_tmp = mgr.getHandle(h1.getName());
+  EXPECT_EQ(h1.getName(), h1_tmp.getName());
+  EXPECT_EQ(h1.getValue(), h1_tmp.getValue());
+
+  HandleType h2_tmp = mgr.getHandle(h2.getName());
+  EXPECT_EQ(h2.getName(), h2_tmp.getName());
+  EXPECT_EQ(h2.getValue(), h2_tmp.getValue());
+
+  // Trying to get a handle to a non-existant resource should throw
+  EXPECT_THROW(mgr.getHandle("no_resource"), HardwareInterfaceException);
+}
+
+TEST_F(ResourceManagerTest, HandleRewriting)
+{
+  // Populate resource manager
+  ResourceManager<HandleType> mgr;
+  mgr.registerHandle(h1);
+
+  // Check handle velues
+  {
+    EXPECT_NO_THROW(mgr.getHandle(h1.getName()));
+    HandleType h1_tmp = mgr.getHandle(h1.getName());
+    EXPECT_EQ(h1.getValue(), h1_tmp.getValue());
+  }
+
+  // Re-register resource: same name, different value
+  HandleType h3(h1.getName(), h2.getValue());
+  mgr.registerHandle(h3);
+
+  // Check that there is indeed only one registered resource
+  vector<string> names = mgr.getNames();
+  EXPECT_EQ(1, names.size());
+  EXPECT_TRUE(find(names.begin(), names.end(), h1.getName()) != names.end());
+
+  // Get handle and check that it now contains a different value
+  {
+    EXPECT_NO_THROW(mgr.getHandle(h1.getName()));
+    HandleType h3_tmp = mgr.getHandle(h1.getName());
+    EXPECT_EQ(h2.getValue(), h3_tmp.getValue());
+    EXPECT_NE(h1.getValue(), h3_tmp.getValue());
+  }
+}
+
+TEST_F(ResourceManagerTest, ResourceClaims)
+{
+  // Default: Manager that does not claim resources
+  {
+    ResourceManager<HandleType> mgr;
+    mgr.registerHandle(h1);
+    mgr.registerHandle(h2);
+
+    EXPECT_TRUE(mgr.getClaims().empty());
+
+    // Getting a handle is the resource-claiming operation (which in this case should not claim anything)
+    mgr.getHandle(h1.getName());
+    mgr.getHandle(h2.getName());
+    mgr.getHandle(h2.getName()); // Getting twice the same resource
+
+    EXPECT_TRUE(mgr.getClaims().empty());
+  }
+
+  // Explicit setting: Manager that does not claim resources
+  {
+    ResourceManager<HandleType, DontClaimResources> mgr;
+    mgr.registerHandle(h1);
+    mgr.registerHandle(h2);
+    EXPECT_TRUE(mgr.getClaims().empty());
+
+    // Getting a handle is the resource-claiming operation (which in this case should not claim anything)
+    mgr.getHandle(h1.getName());
+    mgr.getHandle(h2.getName());
+    mgr.getHandle(h2.getName()); // Getting twice the same resource
+
+    EXPECT_TRUE(mgr.getClaims().empty());
+  }
+
+  // Explicit setting: Manager that does claim resources
+  {
+    ResourceManager<HandleType, ClaimResources> mgr;
+    mgr.registerHandle(h1);
+    mgr.registerHandle(h2);
+    EXPECT_TRUE(mgr.getClaims().empty());
+
+    // Getting a handle is the resource-claiming operation (which in this case should inded claim resources)
+    mgr.getHandle(h1.getName());
+    mgr.getHandle(h2.getName());
+    mgr.getHandle(h2.getName()); // Getting twice the same resource, claims list should still be of size 2
+
+    set<string> claims = mgr.getClaims();
+    EXPECT_EQ(2, claims.size());
+    EXPECT_TRUE(find(claims.begin(), claims.end(), h1.getName()) != claims.end());
+    EXPECT_TRUE(find(claims.begin(), claims.end(), h2.getName()) != claims.end());
+  }
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
