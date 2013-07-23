43c4614782cffb07b6ae254f20d4accde5dc7c48
Dave Coleman <davetcoleman@gmail.com>
2013-07-23 13:54:04 -0700

    Duplicated urdfdom's method of including tinyxml

---
+++ transmission_interface/cmake/SearchForTinyXML.cmake
@@ -0,0 +1,20 @@
+find_library(tinyxml_library tinyxml)
+if (tinyxml_library)
+  message (STATUS "Looking for libtinyxml - found")
+  set(tinyxml_libraries ${tinyxml_library})
+endif ()
+
+find_path(tinyxml_include_dirs NAMES tinyxml.h PATH_SUFFIXES tinyxml)
+if (NOT tinyxml_include_dirs)
+   message (STATUS "Looking for tinyxml/tinyxml.hpp or tinyxml/tinyxml.h - not found.")
+endif ()
+
+if (NOT tinyxml_include_dirs OR NOT tinyxml_libraries)
+   include (FindPkgConfig)
+   if (PKG_CONFIG_FOUND)
+     # Find tinyxml
+     pkg_check_modules(tinyxml tinyxml)
+   else()
+     MESSAGE("Missing: tinyxml")
+   endif()
+endif ()
