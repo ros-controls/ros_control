fc0a773206cb4df22b8bdcadcca877b47d99ad71
Dave Coleman <davetcoleman@gmail.com>
2013-07-23 14:30:06 -0700

    Changed captilization of vars to match cmake standards

--- transmission_interface/cmake/SearchForTinyXML.cmake
+++ transmission_interface/cmake/SearchForTinyXML.cmake
@@ -1,15 +1,15 @@
 find_library(tinyxml_library tinyxml)
 if (tinyxml_library)
   message (STATUS "Looking for libtinyxml - found")
-  set(tinyxml_libraries ${tinyxml_library})
+  set(tinyxml_LIBRARIES ${tinyxml_library})
 endif ()
 
-find_path(tinyxml_include_dirs NAMES tinyxml.h PATH_SUFFIXES tinyxml)
-if (NOT tinyxml_include_dirs)
+find_path(tinyxml_INCLUDE_DIRS NAMES tinyxml.h PATH_SUFFIXES tinyxml)
+if (NOT tinyxml_INCLUDE_DIRS)
    message (STATUS "Looking for tinyxml/tinyxml.hpp or tinyxml/tinyxml.h - not found.")
 endif ()
 
-if (NOT tinyxml_include_dirs OR NOT tinyxml_libraries)
+if (NOT tinyxml_INCLUDE_DIRS OR NOT tinyxml_LIBRARIES)
    include (FindPkgConfig)
    if (PKG_CONFIG_FOUND)
      # Find tinyxml
