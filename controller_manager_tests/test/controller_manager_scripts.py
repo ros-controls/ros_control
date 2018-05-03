3aab399b3a90eb777b59cfd52f29e2ba735cdaa9
Yong Li <yli@clearpath.ai>
2018-05-03 16:48:48 -0400

    Add controller_group script that allows switching groups easily
    The groups are read from ROS parameter "controller_groups"

--- controller_manager_tests/test/controller_manager_scripts.py
+++ controller_manager_tests/test/controller_manager_scripts.py
@@ -23,6 +23,10 @@ reload_response = 'Restore: False\nSuccessfully reloaded libraries\n'
 def run_cm(*args):
     return subprocess.check_output('rosrun controller_manager controller_manager ' + ' '.join(args), shell=True)
 
+# run controller_group in process
+def run_cg(*args):
+    return subprocess.check_output('rosrun controller_manager controller_group ' + ' '.join(args), shell=True)
+
 # helper function that return the actual and expected response
 def load_c(name):
     return run_cm('load', name), loaded_fmt % name
@@ -41,6 +45,7 @@ def spawn_c(name):
 
 def kill_c(name):
     return run_cm('kill', name), stopped_fmt % name + unloaded_fmt % name
+
 
 class TestUtils(unittest.TestCase):
     def test_scripts(self):
@@ -106,6 +111,36 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(run_cm('list'), no_controllers)
 
         self.assertEqual(run_cm('reload-libraries'), reload_response)
+
+        # controller_group
+        # list
+        self.assertEqual(
+            run_cg('list'),
+            'Number of groups: 3\n'
+            '  group1\n      my_controller1\n      my_controller3\n'
+            '  group2\n      my_controller2\n      my_controller3\n'
+            '  group3\n      my_controller1\n      my_controller2\n')
+
+        # spawn
+        self.assertEqual(
+            run_cg('spawn group1'),
+            'Loaded my_controller1\n'
+            'Loaded my_controller3\n'
+            'Started [\'my_controller1\'] successfully\n'
+            'Started [\'my_controller3\'] successfully\n')
+
+        # switch
+        self.assertEqual(
+            run_cg('switch group2'),
+            'Loaded my_controller2\n'
+            'Started [\'my_controller2\'] successfully\n'
+            'Stopped [\'my_controller1\'] successfully\n')
+
+        # switch to faulty group because my_controller1 conflicts with my_controller2
+        self.assertEqual(
+            run_cg('switch group3'),
+            'Error when starting [\'my_controller1\'] and stopping [\'my_controller3\']\n')
+
 
 if __name__ == '__main__':
     import rostest
