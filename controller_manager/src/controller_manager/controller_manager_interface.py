c1cf0d256f1d3970f2ffdb3775c56c089ab9c750
Bence Magyar <bence.magyar.robotics@gmail.com>
2018-08-05 21:11:27 +0000

    back to Python3 prints, add '-s to remaining places

--- controller_manager/src/controller_manager/controller_manager_interface.py
+++ controller_manager/src/controller_manager/controller_manager_interface.py
@@ -8,7 +8,7 @@ def list_controller_types():
     s = rospy.ServiceProxy('controller_manager/list_controller_types', ListControllerTypes)
     resp = s.call(ListControllerTypesRequest())
     for t in resp.types:
-        rospy.loginfo(t)
+        print(t)
 
 
 def reload_libraries(force_kill, restore = False):
@@ -19,16 +19,16 @@ def reload_libraries(force_kill, restore = False):
     load_srv = rospy.ServiceProxy('controller_manager/load_controller', LoadController)
     switch_srv = rospy.ServiceProxy('controller_manager/switch_controller', SwitchController)
 
-    rospy.loginfo("Restore:" + str(restore))
+    print("Restore: " + str(restore))
     if restore:
         originally = list_srv.call(ListControllersRequest())
 
     resp = s.call(ReloadControllerLibrariesRequest(force_kill))
     if resp.ok:
-        rospy.loginfo("Successfully reloaded libraries")
+        print("Successfully reloaded libraries")
         result = True
     else:
-        rospy.logerr("Failed to reload libraries. Do you still have controllers loaded?")
+        print("Failed to reload libraries. Do you still have controllers loaded?")
         result = False
 
     if restore:
@@ -38,7 +38,7 @@ def reload_libraries(force_kill, restore = False):
         switch_srv(start_controllers=to_start,
                    stop_controllers=[],
                    strictness=SwitchControllerRequest.BEST_EFFORT)
-        rospy.loginfo("Controllers restored to original state")
+        print("Controllers restored to original state")
     return result
 
 
@@ -47,11 +47,11 @@ def list_controllers():
     s = rospy.ServiceProxy('controller_manager/list_controllers', ListControllers)
     resp = s.call(ListControllersRequest())
     if len(resp.controller) == 0:
-        rospy.logwarn("No controllers are loaded in mechanism control")
+        print("No controllers are loaded in mechanism control")
     else:
         for c in resp.controller:
             hwi = list(set(r.hardware_interface for r in  c.claimed_resources))
-            rospy.loginfo("'%s' - '%s' ( %s )" % (c.name, "+".join(hwi), c.state))
+            print("'%s' - '%s' ( %s )" % (c.name, "+".join(hwi), c.state))
 
 
 def load_controller(name):
@@ -59,10 +59,10 @@ def load_controller(name):
     s = rospy.ServiceProxy('controller_manager/load_controller', LoadController)
     resp = s.call(LoadControllerRequest(name))
     if resp.ok:
-        rospy.loginfo("Loaded" + name)
+        print("Loaded \'" + name + "\'")
         return True
     else:
-        rospy.logerr("Error when loading" + name)
+        print("Error when loading \'" + name + "\'")
         return False
 
 
@@ -71,10 +71,10 @@ def unload_controller(name):
     s = rospy.ServiceProxy('controller_manager/unload_controller', UnloadController)
     resp = s.call(UnloadControllerRequest(name))
     if resp.ok == 1:
-        rospy.loginfo("Unloaded '%s' successfully" % name)
+        print("Unloaded \'" + name + "\' successfully")
         return True
     else:
-        rospy.logerr("Error when unloading '%s'" % name)
+        print("Error when unloading \'" + name + "\'")
         return False
 
 
@@ -101,10 +101,10 @@ def start_stop_controllers(start_controllers=[], stop_controllers=[]):
     resp = s.call(SwitchControllerRequest(start_controllers, stop_controllers, strictness))
     if resp.ok == 1:
         if start_controllers:
-            rospy.loginfo("Started {} successfully".format(start_controllers))
+            print("Started {} successfully".format(start_controllers))
         if stop_controllers:
-            rospy.loginfo("Stopped {} successfully".format(stop_controllers))
+            print("Stopped {} successfully".format(stop_controllers))
         return True
     else:
-        rospy.logerr("Error when starting {} and stopping {}".format(start_controllers, stop_controllers))
+        print("Error when starting {} and stopping {}".format(start_controllers, stop_controllers))
         return False
